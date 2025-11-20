7342aab37996175640fcdaf733128f061785fe73
Aswun <aswinlutfian32@gmail.com>
2025-11-20 13:48:07 +0700

    menambahkan HorizontalDivider

--- app/src/main/java/com/example/prak8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/FormSiswa.kt
@@ -1,7 +1,11 @@
 package com.example.prak8.view.uicontroller
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -9,7 +13,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.prak8.R
 
 @Composable
@@ -45,6 +51,13 @@ fun FormSiswa (
                         onValueChange = {
                             txtNama = it
                         }
+                    )
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .width(250.dp),
+                        thickness = dimensionResource(id = R.dimen.thickness_divider),
+                        color = Color.Blue
                     )
                 }
             }
