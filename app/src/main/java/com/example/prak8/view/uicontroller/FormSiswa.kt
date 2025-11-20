9430288f705b75fd333e8dc4b7a7baad52b25afa
Aswun <aswinlutfian32@gmail.com>
2025-11-20 14:05:24 +0700

    menambahkan outlinedTextField

--- app/src/main/java/com/example/prak8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/FormSiswa.kt
@@ -4,93 +4,98 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 import com.example.prak8.R
 
-@Composable
-fun FormSiswa() {
 
-    //edit 2 : tambahkan 4 variabel dibawah ini
-    var txtNama by rememberSaveable { mutableStateOf("") }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var txtNama by remember { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
-    val listData: MutableList<String> = mutableListOf(txtNama, txtAlamat, txtGender)
 
     Scaffold(
+        modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(R.string.app_name), color = Color.White) },
+                title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = colorResource(id = R.color.purple_500)
                 )
             )
         }
-    ) { isiRuang ->
+    ){ isiruang ->
+
         Column(
-            modifier = Modifier
-                .padding(isiRuang)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.padding(paddingValues = isiruang),
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            // ➤ TextField Nama
             OutlinedTextField(
                 value = txtNama,
-                onValueChange = { txtNama = it },
                 singleLine = true,
-                label = { Text("Nama Lengkap") },
+                shape = MaterialTheme.shapes.medium,
                 modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
-                shape = MaterialTheme.shapes.medium
+                    .width(250.dp)
+                    .padding(top = 20.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = { txtNama = it }
             )
 
             HorizontalDivider(
                 modifier = Modifier
                     .padding(12.dp)
                     .width(250.dp),
-                thickness = dimensionResource(id = R.dimen.thickness_divider),
+                thickness = 1.dp,
                 color = Color.Blue
             )
+            Row {
+                pilihanJK.forEach { item ->
 
-            // ➤ Radio Button JK
-            Row(
-                modifier = Modifier.padding(top = 10.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                pilihanJk.forEach { item ->
                     Row(
-                        modifier = Modifier
-                            .selectable(
-                                selected = txtGender == item,
-                                onClick = { txtGender = item }
-                            )
-                            .padding(end = 16.dp),
+                        modifier = Modifier.selectable(
+                            selected = txtGender == item,
+                            onClick = { txtGender = item }
+                        ),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
+
                         RadioButton(
                             selected = txtGender == item,
                             onClick = { txtGender = item }
                         )
-                        Text(item)
+
+                        Text(text = item)
                     }
                 }
             }
+
             HorizontalDivider(
                 modifier = Modifier
                     .padding(5.dp)
                     .width(250.dp),
-                thickness = dimensionResource(id = R.dimen.thickness_divider),
+                thickness = 1.dp,
                 color = Color.Blue
             )
+            OutlinedTextField(
+                value = txtAlamat,
+                singleLine = true,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp),
+                label = { Text(text = "Alamat Lengkap") },
+                onValueChange = { txtAlamat = it }
+            )
+
         }
     }
 }
