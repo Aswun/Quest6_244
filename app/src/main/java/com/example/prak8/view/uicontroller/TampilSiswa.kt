8e6c6cd44a61d3a4ebffd872e4834fa051a0bd7e
Aswun <aswinlutfian32@gmail.com>
2025-11-20 14:09:33 +0700

    membuat TampilSiswa.kt pada package view.uicontroller

---
+++ app/src/main/java/com/example/prak8/view/uicontroller/TampilSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.prak8.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked: () -> Unit
+) {
+
+}
