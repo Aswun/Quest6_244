8b6788e4cf38f23204721419d99529a073897b48
Aswun <aswinlutfian32@gmail.com>
2025-11-20 14:23:22 +0700

    menambahkan navigasi FormSiswa

--- app/src/main/java/com/example/prak8/Navigasi.kt
+++ app/src/main/java/com/example/prak8/Navigasi.kt
@@ -6,11 +6,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.prak8.viewmodel.SiswaViewModel
+import com.example.prak8.model.DataJK
+import com.example.prak8.view.uicontroller.FormSiswa
+import com.example.prak8.view.uicontroller.TampilSiswa
 
 enum class Navigasi {
     Formulir,
@@ -33,6 +38,19 @@ fun SiswaApp(
             startDestination = Navigasi.Formulir.name,
             modifier = Modifier.padding(isiRuang)
         ) {
+            composable(route = Navigasi.Formulir.name) {
 
+                val konteks = LocalContext.current
+
+                FormSiswa(
+                    pilihanJK = DataJK.JenisK.map { id ->
+                        konteks.resources.getString(id)
+                    },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
         }
 }
